fb5e311e25c1d60635d8d45761b2d33aa547d84c
Jonny-Cho <andante2183@gmail.com>
2021-11-25 22:11:00 +0900

    step10: 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-advanced-aop
---
+++ src/main/java/hello/aop/SpringAdvancedAopApplication.java
@@ -0,0 +1,13 @@
+package hello.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedAopApplication.class, args);
+    }
+
+}
