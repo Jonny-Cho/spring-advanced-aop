0f23e4b06192ee30bec126e411746d6790701142
Jonny-Cho <andante2183@gmail.com>
2021-12-06 21:30:05 +0900

    step12: 재시도 AOP

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ public class ExamRepository {
      * 5번에 1번 실패하는 요청
      */
     @Trace
+    @Retry(value = 4)
     public String save(final String itemId) {
         seq++;
         if (seq % 5 == 0) {
